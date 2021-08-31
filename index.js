f8f12f9c3ebaf2c3cc7b16a67c8a1f7ca15c1f2d
nikolayhotim1 <hotim_nikolay@mail.ru>
2021-08-31 23:48:51 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+'usestrict';
+let chevy = {
+    make: 'Chevy',
+    model: 'Bel Air',
+    year: 1957,
+    color: 'red',
+    passengers: 2,
+    convertible: false,
+    mileage: 1021,
+    maxSpeed: 193,
+    horsesPower: 283,
+    engineCapacity: 4.636,
+    turnOn: true,
+    bodyType: 'sedan'
+};
